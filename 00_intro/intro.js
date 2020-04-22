adbbf52c496007c251c033c08d5fb71ed4ac8f02
gustavo <gustavocepeda9903@gmail.com>
2020-04-22 12:29:19 -0400

    Agregando README

---
+++ 00_intro/intro.js
@@ -0,0 +1,17 @@
+const inputcolor = document.querySelector('#color');
+const btncambiarcolor = document.querySelector('#cambiar_color');
+
+btncambiarcolor.addEventListener('click', () => {
+ let color = inputcolor.value;
+
+ if (color === ''){
+     alert("debes escribir un valor el color");
+ } else{
+     cambiarcolorfondo(color);
+ }
+ 
+});
+
+function cambiarcolorfondo(color) {
+    document.body.style.backgroundColor = color;
+}
